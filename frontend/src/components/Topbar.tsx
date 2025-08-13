8c0c1cd56f77bf8a47c1d7931d80f90e5626f462
Anmol Kumar <thenameisanmol6@gmail.com>
2025-08-13 23:15:41 +0530

    logo update

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -17,7 +17,7 @@ const Topbar = () => {
     '
 		>
 			<div className='flex gap-2 items-center'>
-				<img src='/logo-song.png' className='size-8' alt='Spotify logo' />
+				<img src='/sangeetify.png' className='size-8' alt='Sangeetify logo' />
 				Sangeetify
 			</div>
 			<div className='flex items-center gap-4'>
